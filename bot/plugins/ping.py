5835509b368b83c6c3315c5032c1f9fe4a7217ee
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2022-08-30 18:17:42 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,12 +13,12 @@ async def ping_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
+        text="<b>Hi, I am Rename Bot!</b>\n\n"
              "I can rename media without downloading it!\n"
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.",
         reply_markup=types.InlineKeyboardMarkup([[
-           types.InlineKeyboardButton("Show Settings",
+           types.InlineKeyboardButton("Settings",
                                       callback_data="showSettings")
         ]])
     )
@@ -35,7 +35,8 @@ async def help_handler(c: Client, m: "types.Message"):
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.\n\n"
              "To set custom thumbnail reply to any image with /set_thumbnail\n\n"
-             "To see custom thumbnail press /show_thumbnail",
+             "To see custom thumbnail press /show_thumbnail\n\n"
+             "Join On [My Updates Channel](https://t.me/myownbots) For More.",
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Show Settings",
                                       callback_data="showSettings")]])
